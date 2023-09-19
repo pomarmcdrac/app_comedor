9235b64d2cfdab6275f511aaa3cbf5866cbe4049
Omar Morales <pomaral@live.com.mx>
2023-09-18 22:19:09 -0600

    Cargo proyecto a git

--- lib/main.dart
+++ lib/main.dart
@@ -27,8 +27,6 @@ class _MyAppState extends State<MyApp> {
     super.initState();
   }
 
-
-
   @override
   Widget build(BuildContext context) {
     SystemChrome.setPreferredOrientations([ DeviceOrientation.portraitUp, DeviceOrientation.portraitDown, ]);
